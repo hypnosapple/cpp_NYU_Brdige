6d809f8885cb1b99c6f2913694bb953dd07f62e1
Hongxin Li <vidorxin@gmail.com>
2024-02-04 15:46:20 -0500

    2021winter

--- Practice Exam01/2021Winter/2021winter_q12/2021winter_q12.cpp
+++ Practice Exam01/2021Winter/2021winter_q12/2021winter_q12.cpp
@@ -4,15 +4,43 @@
 #include <iostream>
 using namespace std;
 const int foot = 12;
-const int yard = 3;
+const int yard = 36;
 
 int main()
 {
     int n;
+    int s; // sum
+    int d;
     bool endloop;
+    int f, y, i;
 
     endloop = false;
+    d = 1;
+    s = 0;
 
-    std::cout << "Hello World!\n";
+    //calculate the sum of the distance
+    while (endloop == false) {
+        cout << "Enter the number of inches the snail traveled in the day #" << d << endl;
+        cout << "or type -1 if they reached their destination: " << endl;
+        cin >> n;
+
+        if (n == -1) {
+            endloop = true;
+        }
+        else {
+            s += n;
+            //update day count
+            d++;
+        }
+    }
+
+    //convert the distance 
+    y = s / 36;
+    s = s % 36;
+    f = s / 12;
+    i = s % 12;
+
+    cout << "In " << d << "days, the snail traveled " << y << " yards, " << f << " feet and " << i << " inches." << endl;
+
 }
 
