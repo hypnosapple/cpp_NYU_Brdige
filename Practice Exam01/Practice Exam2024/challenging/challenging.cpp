9a452e3533abb0487a6f664a6449fa7d2e837e4f
Hongxin Li <vidorxin@gmail.com>
2024-02-05 17:42:15 -0500

    challenge fix

--- Practice Exam01/Practice Exam2024/challenging/challenging.cpp
+++ Practice Exam01/Practice Exam2024/challenging/challenging.cpp
@@ -6,93 +6,30 @@ using namespace std;
 
 int main()
 {
-    int n;
-    int l;
-    int a, b, c;
-    int mc;
-    int counterLspace;
-    int counterRspace;
-    int counterMspace;
-    int counterMMspace;
-    int counterLLspace;
-    int counterLRspace;
+	char frame = '#';
+	char planks = '$';
+	char space = ' ';
+	int input;
 
-    cout << "Please enter a positive integer: " << endl; 
-    cin >> n;
-                                            
-    counterLspace = 1;
-    counterRspace = 1;
-    counterMspace = n;
-    counterMMspace = 2;
-    counterLLspace = (n-4)/2;
-    counterLRspace = (n - 4) / 2;
+	cout << "Please enter a positive integer. " << endl;
+	cin >> input; 
 
-    for (l = 1; l <= (2 * n) - 1; l++) {
+	//print by each row (up to down)
+	for (int row = 1; row <= input; row++) {
+		for (int column = 1; column <= input; column++) {
+			
+			if (row == 1 || row == input || column == 1 || column == input) {
+				cout << frame;
+			}
+			else if (row == column || row + column == input + 1) {
+				cout << planks;
+			}
+			else {
+				cout << space;
+			}
+		}
 
-        //print the first and last line
-        if (l == 1 || l == (2 * n - 1)) {
-            for (a = 1; a <= n; a++) {
-                cout << "#";
-            }
-        } //print second and last second line
-        else if (l == 3 || l == (2*n-3) ){
-            cout << "#$";
-            for (c = 1; c <= n - 4; c++) {
-                cout << " ";
-            }
-            cout << "$#";
-        } //print first a for each odd line
-        else if (l % 2 == 1) {
-            cout << "#";
-            //print upper left space
-            if (l >= 5 && l <= n - 1) {
-                for (c = 1; c <= counterLspace; c++) {
-                    cout << " ";
-                }
-                cout << "$";
-                counterLspace++;
-
-                //print upper middle space
-                for (mc = 1; mc <= counterMspace - 6; mc++) {
-                    cout << " ";
-                }
-                cout << "$";
-                counterMspace -= 2;
-
-                //print the upper right space
-                for (c = 1; c <= counterRspace; c++) {
-                    cout << " ";
-                }
-                counterRspace++;
-            } //print lower part
-            else if (l > n && l < (2 * n - 3)) {
-                //print lower left space
-                for (c = counterLLspace; c > 0; c--) {
-                    cout << " ";
-                }
-                counterLLspace--;
-                cout << "$";
-
-                //print lower middle space
-                if (l >= n + 2) {
-                    for (mc = 1; mc <= counterMMspace; mc++) {
-                        cout << " ";
-                    }
-                    counterMMspace += 2;
-                }
-                
-                cout << "$";
-
-                //print lower right space
-                for (c = counterLRspace; c > 0; c--) {
-                    cout << " ";
-                }
-                counterLRspace--;
-            }
-            cout << "#";
-        }  
-
-        cout << endl;
-    }
+		cout << endl;
+	}
 }
 
